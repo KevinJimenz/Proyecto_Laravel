a57d4b548fc00bf2b56e5aec0db30610ccf879fa
Kav <kevincamilo56@gmail.com>
2024-08-05 07:11:14 -0500

    Actualizo el diseño del sidebar en la plantilla principal

--- Carcel/resources/views/layouts/app.blade.php
+++ Carcel/resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 </head>
 
@@ -37,7 +37,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion barra_lateral" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center " href="{{ URL::to('home') }}">
@@ -110,7 +110,7 @@
                     <i class="fas fa-fw fa-file-alt"></i>
 
 
-                    <span>Reportes</span>
+                    <span class="reportes">Reportes</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
